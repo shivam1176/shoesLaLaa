7260e71e9d4e4b709935b20e39618ba5a1202df3
shivam1176 <83699416+shivam1176@users.noreply.github.com>
2024-01-20 17:30:34 +0530

    cummit initial files

---
+++ src/data.js
@@ -0,0 +1,218 @@
+let shopItemsData = [
+
+  {
+    id: "img1",
+    name: "JORDAN 1",
+    price: 8000,
+    desc: "",
+    img: "../nike/img/cb881e4c-520f-475f-9694-0c2c57dfca45.webp",
+  },
+  {
+    id: "img2",
+    name: "DUNK LOW RETRO",
+    price: 5530,
+    desc: "",
+    img: "../nike/img/df0dbd6e-9e27-4b46-b6b4-7c1c93ee1161.webp",
+  },
+  {
+    id: "img3",
+    name: "NIKE AIR MAX solo",
+    price: 10000,
+    desc: "",
+    img: "../nike/img/940d803f-91b0-4cc9-8976-477835cf3778.webp",
+  },
+  {
+    id: "img4",
+    name: "FREAK 5 EP",
+    price: 30000,
+    desc: "",
+    img: "../nike/img/5bc498c6-9b7c-41f5-ad2c-e0ac871a13ff.webp",
+  },
+
+
+
+  {
+    id: "img5",
+    name: "ducks",
+    price: 3000,
+    desc: "",
+    img: "../nike/img/0124a594-d4ed-40c4-80b9-f0d79e821441.webp",
+  },
+  {
+    id: "img6",
+    name: "Air jordan",
+    price: 5000,
+    desc: "",
+    img: "../nike/img/dae5b2b3-4234-4d15-8065-8c22605dc1d1.webp",
+  },
+  {
+    id: "img7",
+    name: "Air jordan",
+    price: 9000,
+    desc: "",
+    img: "../nike/img/8ba3216b-1961-453e-9407-685cd6b3360d.webp",
+  },
+
+
+  {
+    id: "img8",
+    name: "NIKE AIR MAX solo",
+    price: 6000,
+    desc: "",
+    img: "../nike/img/disuwww5uxkqulu2aivh.webp",
+  },
+
+
+
+  {
+    id: "img9",
+    name: "Air jordan",
+    price: 7000,
+    desc: "",
+    img: "../nike/img/0a3e7846-fdab-4cf8-becf-638ec3ad9d3d.webp",
+  },
+  {
+    id: "img10",
+    name: "NIKE AIR MAX",
+    price: 5000,
+    desc: "",
+    img: "../nike/img/d1acc841-5bd4-4a06-811a-95711e9c7a73.webp",
+  },
+  {
+    id: "img11",
+    name: "JORDAN 1",
+    price: 8060,
+    desc: "",
+    img: "../nike/img/d3123e79-53e5-4a03-aa5b-fbc5c18b9bfd.webp",
+  },
+  {
+    id: "img12",
+    name: "DUNK LOW RETRO",
+    price: 7000,
+    desc: "",
+    img: "../nike/img/nocta-hot-step-air-terra-shoes-zwfDgC.jpg",
+  },
+  {
+    id: "img13",
+    name: "NIKE AIR Force 1",
+    price: 4000,
+    desc: "",
+    img: "../nike/img/6ef8f54a-1dda-49b8-9097-34c9c3dc766e.webp",
+  },
+
+
+
+
+  {
+    id: "img14",
+    name: "DUNK LOW RETRO",
+    price: 8000,
+    desc: "",
+    img: "../nike/img/92357ec8-026e-439f-973f-d1224fa8d21f.webp",
+  },
+  {
+    id: "img15",
+    name: "NIKE AIR MAX solo",
+    price: 9000,
+    desc: "",
+    img: "../nike/img/15470396-cbdc-49f1-978d-479b0b4aadda.webp",
+  },
+  {
+    id: "img16",
+    name: "NIKE AIR MAX",
+    price: 8000,
+    desc: "",
+    img: "../nike/img/b212e5bf-ec2f-4b0d-b759-37e893003043.webp",
+  },
+  {
+    id: "img17",
+    name: "JORDAN 1",
+    price: 1000,
+    desc: "",
+    img: "../nike/img/b6781198-fb6b-4438-b6ec-a2dec96f9a14.webp",
+  },
+  {
+    id: "img18",
+    name: "DUNK LOW RETRO",
+    price: 3000,
+    desc: "",
+    img: "../nike/img/ce38e3d4-7382-4d01-8984-3c5667f7e465.webp",
+  },
+
+
+
+  {
+    id: "img19",
+    name: "NIKE AIR MAX solo",
+    price: 5000,
+    desc: "",
+    img: "../nike/img/30927f64-1c21-4475-a730-5c48e931d1b7.webp",
+  },
+  {
+    id: "img20",
+    name: "NIKE AIR MAX",
+    price: 9000,
+    desc: "",
+    img: "../nike/img/011532a3-714d-458c-a520-1167d5cb4d77.webp",
+  },
+  {
+    id: "img21",
+    name: "JORDAN 1",
+    price: 3000,
+    desc: "",
+    img: "../nike/img/8409c3b2-add4-4b18-927f-2767435a660b.webp",
+  },
+  {
+    id: "img22",
+    name: "DUNK LOW RETRO",
+    price: 4000,
+    desc: "",
+    img: "../nike/img/7095aee1-5ce4-47fc-87c8-9408586b2d69.webp",
+},
+
+    {
+      id: "img23",
+      name: "NIKE AIR MAX solo",
+      price: 2000,
+      desc: "",
+      img: "../nike/img/2978f86c-fd3d-4f3d-9651-a8c559252652.webp",
+    },
+    {
+      id: "img24",
+      name: "NIKE AIR MAX",
+      price: 5000,
+      desc: "",
+      img: "../nike/img/93eb5c15-7378-439a-a234-3bf3fe542f6d.webp",
+    },
+    {
+      id: "img25",
+      name: "JORDAN 1",
+      price: 6000,
+      desc: "",
+      img: "../nike/img/74e02a76-fe86-48e0-bd76-2993548666a3.webp",
+    },
+    {
+      id: "img26",
+      name: "DUNK LOW RETRO",
+      price: 8000,
+      desc: "",
+      img: "../nike/img/47a469f4-0497-45c7-89ec-fbe838e76e17.webp",
+  },
+
+  {
+    id: "img27",
+    name: "JORDAN 1",
+    price: 9000,
+    desc: "",
+    img: "../nike/img/44cafef0-7faf-4178-a6fd-be84bb0b2da4.webp",
+  },
+  {
+    id: "img27",
+    name: "DUNK LOW RETRO",
+    price: 4000,
+    desc: "",
+    img: "../nike/img/9666574d-f12c-46c8-b04b-fc50b3581e91.webp",
+},
+
+
+];
---
+++ src/data2.js
@@ -0,0 +1,218 @@
+let shopItemsData = [
+
+  {
+    id: "img29",
+    name: "JORDAN 1",
+    price: 8000,
+    desc: "",
+    img: "../nike/img/0e71e52d-be87-4397-aeb7-1d528a8f6513.webp",
+  },
+  {
+    id: "img30",
+    name: "DUNK LOW RETRO",
+    price: 5530,
+    desc: "",
+    img: "../nike/img/92357ec8-026e-439f-973f-d1224fa8d21f.webp",
+  },
+  {
+    id: "img31",
+    name: "NIKE AIR MAX solo",
+    price: 10000,
+    desc: "",
+    img: "../nike/img/011532a3-714d-458c-a520-1167d5cb4d77.webp",
+  },
+  {
+    id: "img32",
+    name: "FREAK 5 EP",
+    price: 30000,
+    desc: "",
+    img: "../nike/img/215da21f-3cad-401c-a523-e199c83c7cbe.webp",
+  },
+
+
+
+  {
+    id: "img33",
+    name: "ducks",
+    price: 3000,
+    desc: "",
+    img: "../nike/img/9dadce8a-77aa-4317-8713-63d820c0dac9.webp",
+  },
+  {
+    id: "img34",
+    name: "Air jordan",
+    price: 5000,
+    desc: "",
+    img: "../nike/img/09a50d1d-66c6-4a5b-8e56-8ec1e94a941d.webp",
+  },
+  {
+    id: "img7",
+    name: "Air jordan",
+    price: 9000,
+    desc: "",
+    img: "../nike/img/8ba3216b-1961-453e-9407-685cd6b3360d.webp",
+  },
+
+
+  {
+    id: "img8",
+    name: "NIKE AIR MAX solo",
+    price: 6000,
+    desc: "",
+    img: "../nike/img/disuwww5uxkqulu2aivh.webp",
+  },
+
+
+
+  {
+    id: "img9",
+    name: "Air jordan",
+    price: 7000,
+    desc: "",
+    img: "../nike/img/0a3e7846-fdab-4cf8-becf-638ec3ad9d3d.webp",
+  },
+  {
+    id: "img10",
+    name: "NIKE AIR MAX",
+    price: 5000,
+    desc: "",
+    img: "../nike/img/d1acc841-5bd4-4a06-811a-95711e9c7a73.webp",
+  },
+  {
+    id: "img11",
+    name: "JORDAN 1",
+    price: 8060,
+    desc: "",
+    img: "../nike/img/d3123e79-53e5-4a03-aa5b-fbc5c18b9bfd.webp",
+  },
+  {
+    id: "img12",
+    name: "DUNK LOW RETRO",
+    price: 7000,
+    desc: "",
+    img: "../nike/img/nocta-hot-step-air-terra-shoes-zwfDgC.jpg",
+  },
+  {
+    id: "img13",
+    name: "NIKE AIR Force 1",
+    price: 4000,
+    desc: "",
+    img: "../nike/img/6ef8f54a-1dda-49b8-9097-34c9c3dc766e.webp",
+  },
+
+
+
+
+  {
+    id: "img14",
+    name: "DUNK LOW RETRO",
+    price: 8000,
+    desc: "",
+    img: "../nike/img/92357ec8-026e-439f-973f-d1224fa8d21f.webp",
+  },
+  {
+    id: "img15",
+    name: "NIKE AIR MAX solo",
+    price: 9000,
+    desc: "",
+    img: "../nike/img/15470396-cbdc-49f1-978d-479b0b4aadda.webp",
+  },
+  {
+    id: "img16",
+    name: "NIKE AIR MAX",
+    price: 8000,
+    desc: "",
+    img: "../nike/img/b212e5bf-ec2f-4b0d-b759-37e893003043.webp",
+  },
+  {
+    id: "img17",
+    name: "JORDAN 1",
+    price: 1000,
+    desc: "",
+    img: "../nike/img/b6781198-fb6b-4438-b6ec-a2dec96f9a14.webp",
+  },
+  {
+    id: "img18",
+    name: "DUNK LOW RETRO",
+    price: 3000,
+    desc: "",
+    img: "../nike/img/ce38e3d4-7382-4d01-8984-3c5667f7e465.webp",
+  },
+
+
+
+  {
+    id: "img19",
+    name: "NIKE AIR MAX solo",
+    price: 5000,
+    desc: "",
+    img: "../nike/img/30927f64-1c21-4475-a730-5c48e931d1b7.webp",
+  },
+  {
+    id: "img20",
+    name: "NIKE AIR MAX",
+    price: 9000,
+    desc: "",
+    img: "../nike/img/011532a3-714d-458c-a520-1167d5cb4d77.webp",
+  },
+  {
+    id: "img21",
+    name: "JORDAN 1",
+    price: 3000,
+    desc: "",
+    img: "../nike/img/8409c3b2-add4-4b18-927f-2767435a660b.webp",
+  },
+  {
+    id: "img22",
+    name: "DUNK LOW RETRO",
+    price: 4000,
+    desc: "",
+    img: "../nike/img/7095aee1-5ce4-47fc-87c8-9408586b2d69.webp",
+},
+
+    {
+      id: "img23",
+      name: "NIKE AIR MAX solo",
+      price: 2000,
+      desc: "",
+      img: "../nike/img/2978f86c-fd3d-4f3d-9651-a8c559252652.webp",
+    },
+    {
+      id: "img24",
+      name: "NIKE AIR MAX",
+      price: 5000,
+      desc: "",
+      img: "../nike/img/93eb5c15-7378-439a-a234-3bf3fe542f6d.webp",
+    },
+    {
+      id: "img25",
+      name: "JORDAN 1",
+      price: 6000,
+      desc: "",
+      img: "../nike/img/74e02a76-fe86-48e0-bd76-2993548666a3.webp",
+    },
+    {
+      id: "img26",
+      name: "DUNK LOW RETRO",
+      price: 8000,
+      desc: "",
+      img: "../nike/img/47a469f4-0497-45c7-89ec-fbe838e76e17.webp",
+  },
+
+  {
+    id: "img27",
+    name: "JORDAN 1",
+    price: 9000,
+    desc: "",
+    img: "../nike/img/44cafef0-7faf-4178-a6fd-be84bb0b2da4.webp",
+  },
+  {
+    id: "img28",
+    name: "DUNK LOW RETRO",
+    price: 4000,
+    desc: "",
+    img: "../nike/img/9666574d-f12c-46c8-b04b-fc50b3581e91.webp",
+},
+
+
+];
